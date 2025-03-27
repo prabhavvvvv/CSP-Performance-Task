a42c196648febd467d430392cc6bbbb2fc136501
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2025-03-27 14:32:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-import pygame 
+import turtle as trtl
+p = trtl.Turtle()
 
-pygame.init()
+def main(x, y):
+    x = -10
+    y = 0
+    colors = ["red", "blue", "green", "pink", "yellow"]
+    print(colors)
 
-game_screen = pygame.display.set_mode(600, 600)
+    new_color = "purple"
+    colors.append(new_color)
+    print()
 
-game_running = True
-while game_running:
-    for event in pygame.event.get():
-        if event.type == False:
-            game_running = False
+    if len(colors) > 5:
+        print("There are enough infinity stones")
+    else:
+        colors.append("black")
+
+    while True:
+        color_chooser = input("What color do you want Thanos's infinity gauntlet to be?: ")
+        if color_chooser in colors:
+            print("Now drawing a", color_chooser, "Thanos gauntlet")
+            break
+        else:
+            print("Invalid color choice, please try again")
+
+    p.penup()
+    p.goto(x, y)
+    p.setheading(-90)
+    p.pendown()
+    p.circle(100, extent = 180)
+    p.left(40)
+    p.forward(40)
+    p.right(40)
+
+
+main(-100, 0)
+
+wn = trtl.Screen()
+wn.mainloop()
